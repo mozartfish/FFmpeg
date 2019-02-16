cce4d5c4fbea61fa9c39ecdf9b51ccf1105e753d
THOMAS ADY <u1120386@lab1-10.eng.utah.edu>
2019-02-16 15:36:32 -0700

    Added basic changes to cool.h

---
+++ libavcodec/cool.h
@@ -0,0 +1,14 @@
+
+/*
+  Authors: Thomas Ady && Pranav Rajan
+  CS3505 Spring 2019
+  2/16/19
+
+
+  Format declarations for the cool image format
+ */
+#ifndef AVCODEC_COOL_H
+#define AVCODEC_COOL_H
+
+
+#endif
